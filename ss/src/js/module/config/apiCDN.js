b907d0d8881d982b6399c4fd4736fbabc730a78c
bigui.ding <bigui.ding@ef.com>
2015-09-14 17:58:37 +0800

    resources

--- ss/src/js/module/config/apiCDN.js
+++
@@ -1,8 +0,0 @@
-Ymt.add(function(require, exports, module) {
-	var conf = {
-		"dev": "", //开发  //http://c0.alpha.ymatou.com
-		"alpha": "", //beta
-		"dest": "" //线上
-	}
-	module.exports = conf["dev"];
-});

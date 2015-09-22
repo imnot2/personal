4a9395835626f38ded10ffb1e3f44e5b9f47c493
biggie <510036379@qq.com>
2015-09-22 23:48:36 +0800

    resources

--- ss/src/js/directives/wigets/topBar.js
+++ ss/src/js/directives/wigets/topBar.js
@@ -1,10 +1,10 @@
 directives.directive('wigets.topBar',['$scope',function($scope){
     return {
         scope:{},
-        restrict:'E',
+        restrict:'AE',
         templeteUrl:'../../html/tpls/ui-wiget/topBar.tpl.html',
         controllor:function($scope){
-
+        	$scope.addSideMenu = function(){}
         },
         link:function(scope,elment,attrs){
             

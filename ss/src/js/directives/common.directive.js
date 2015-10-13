9368b4962efc38c37c416797a51f672d1757879d
bigui.ding <bigui.ding@ef.com>
2015-10-13 14:58:48 +0800

    add common directive

---
+++ ss/src/js/directives/common.directive.js
@@ -0,0 +1,22 @@
+directives.directive('login', ['$scope', function () {
+    return {
+        restrict: 'AE',
+        link: function (scope, element, attrs) {
+
+        }
+    }
+}]).directive('register', ['$scope', function () {
+    return {
+        restrict: 'AE',
+        link: function (scope, element, attrs) {
+
+        }
+    }
+}]).directive('goback', ['$scope', function () {
+    return {
+        restrict: 'AE',
+        link: function (scope, element, attrs) {
+
+        }
+    }
+}])
